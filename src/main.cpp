92e470848dd98da11c47a9c5af2157b30b63538c
hainlh <longhai230303@gmail.com>
2022-04-02 21:14:32 +0700

    addtional main change

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <vector>
 #include <SDL.h>
 #include <SDL_image.h>
 
 #include "RenderWindow.h"
-
+#include "Entity.h"
 
 int main(int argc, char* args[])
 {
@@ -11,9 +12,14 @@ int main(int argc, char* args[])
 		std::cout << "HEY SDLLLLLLLL_INIT HAS FAILED. SDL_ERROR: " << SDL_GetError() << std::endl;
 	if (!(IMG_Init(IMG_INIT_PNG)))
 		std::cout << "IMG_Init has failed. Error :" << SDL_GetError() << std::endl;
-	RenderWindow window("GAMEEEEE 1.0", 720, 1080);
+	RenderWindow window("GAMEEEEE 1.0", 1080, 1080);
 
 	SDL_Texture* ball = window.loadTexture("animation/golfballbetterv1.png");
+	// creating background
+
+
+	std::vector <Entity> entities = { Entity(Vector2(135,250),ball) };
+
 
 	bool gameRunning = true;
 	SDL_Event event;
@@ -26,7 +32,10 @@ int main(int argc, char* args[])
 				gameRunning = false;
 		}
 		window.clear();
-		window.render(ball);
+		for (Entity& block : entities) //every block in entities[i] will be call till the end
+		{
+			window.render(block);
+		}
 		window.display();
 	}
 	window.cleanUp();
